7677a8daa2c983ab870b4e3e0b0519addf94cfe0
pnminh <pnminh232@gmail.com>
2021-04-02 15:44:46 -0500

    init README

--- data-modeling/project/workspace/README.md
+++ data-modeling/project/workspace/README.md
@@ -1,2 +1,28 @@
-- [read json contents into panda dataframe](https://realpython.com/pandas-read-write-files/#json-files)
-- [use dataframe with selected columns](https://www.eEarthdatascience.org/courses/intro-to-earth-data-science/scientific-data-structures-python/pandas-dataframes/indexing-filtering-data-pandas-dataframes/#:~:text=This%20feature%20of%20pandas%20dataframes,and%20then%20use%20the%20attribute%20.)
+# Sparkify's song ETL pipeline
+
+## About the company
+
+Sparkify is a company focusing on providing users with the highest quality music collections.
+
+
+## Purpose of the pipeline
+
+Our data team at Sparkify has been working extensively on analyzing the habit our customers on music selection in order to provide them with efficient recommendations on the next songs they like to listen to.
+The data pipeline provides a facilitating tool to extract data from JSON files, transform the data into fact and dimension tables based on STAR schema, and then load those data into Postgres Database.
+## Example queries
+1. Specify the list of playing songs for an user. 
+    ```sql
+    select u.first_name ,u.last_name, s.title from songplays sp
+    join users u
+    on sp.user_id = u.user_id
+    join songs s
+    on sp.song_id = s.song_id ;
+    ```
+2. Look for how popular each song is based on its occurrences in the current song playing list for all users.
+    ```sql
+    select s.title, count(sp.song_id ) as occurrences  from songplays sp
+    join songs s 
+    on sp.song_id = s.song_id
+    group by s.title 
+    order by occurrences;
+    ```
--- data-modeling/project/workspace/sql_queries.py
+++ data-modeling/project/workspace/sql_queries.py
@@ -9,23 +9,23 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 # CREATE TABLES
 
 songplay_table_create = ("""
-CREATE TABLE IF NOT EXISTS songplays(songplay_id text, start_time bigint, user_id int, level text, song_id text, artist_id text, session_id int, location text, user_agent text )
+CREATE TABLE IF NOT EXISTS songplays(songplay_id text PRIMARY KEY, start_time bigint, user_id int, level text, song_id text, artist_id text, session_id int, location text, user_agent text )
 """)
 
 user_table_create = ("""
-CREATE TABLE IF NOT EXISTS users(user_id int, first_name text, last_name text, gender text, level text)
+CREATE TABLE IF NOT EXISTS users(user_id int PRIMARY KEY, first_name text, last_name text, gender text, level text)
 """)
 
 song_table_create = ("""
-CREATE TABLE IF NOT EXISTS songs(song_id text, title text, artist_id text, year int, duration numeric)
+CREATE TABLE IF NOT EXISTS songs(song_id text PRIMARY KEY, title text, artist_id text, year int, duration numeric)
 """)
 
 artist_table_create = ("""
-CREATE TABLE IF NOT EXISTS artists(artist_id text, name text, location text, latitude numeric, longtitude numeric)
+CREATE TABLE IF NOT EXISTS artists(artist_id text PRIMARY KEY, name text, location text, latitude numeric, longtitude numeric)
 """)
 
 time_table_create = ("""
-CREATE TABLE IF NOT EXISTS time(start_time bigint, hour int, day int, week int, month int, year int, weekday int)
+CREATE TABLE IF NOT EXISTS time(start_time bigint PRIMARY KEY, hour int, day int, week int, month int, year int, weekday int)
 """)
 
 # INSERT RECORDS
@@ -36,19 +36,23 @@ INSERT INTO songplays(songplay_id, start_time, user_id, level, song_id, artist_i
 
 user_table_insert = ("""
 INSERT INTO users(user_id, first_name, last_name, gender, level) VALUES(%s,%s,%s,%s,%s)
+ON CONFLICT(user_id) DO NOTHING
 """)
 
 song_table_insert = ("""
 INSERT INTO songs(song_id,title,artist_id,year,duration) VALUES(%s,%s,%s,%s,%s)
+ON CONFLICT(song_id) DO NOTHING
 """)
 
 artist_table_insert = ("""
-INSERT INTO artists(artist_id, name, location, latitude, longtitude) VALUES(%s,%s,%s,%s,%s)
+INSERT INTO artists(artist_id, name, location, latitude, longtitude) VALUES(%s,%s,%s,%s,%s) \
+ON CONFLICT(artist_id) DO NOTHING
 """)
 
 
 time_table_insert = ("""
 INSERT INTO time(start_time, hour, day, week, month, year, weekday) VALUES(%s,%s,%s,%s,%s,%s,%s)
+ON CONFLICT(start_time) DO NOTHING
 """)
 
 # FIND SONGS
