dd4ac76b226fb636f94691f7765346eef697202f
pnminh <pnminh232@gmail.com>
2021-07-06 06:58:44 -0500

    fact and dimension tables

--- data-pipeline/project/workspace/airflow/plugins/operators/load_dimension.py
+++ data-pipeline/project/workspace/airflow/plugins/operators/load_dimension.py
@@ -5,18 +5,33 @@ from airflow.utils.decorators import apply_defaults
 class LoadDimensionOperator(BaseOperator):
 
     ui_color = '#80BD9E'
-
+    dimension_sql_template = """
+            TRUNCATE {dimension_table_name}; 
+            INSERT INTO {dimension_table_name}
+            {dimension_table_insert_query};
+        """
     @apply_defaults
     def __init__(self,
                  # Define your operators params (with defaults) here
                  # Example:
                  # conn_id = your-connection-name
+                 redshift_conn_id="",
+                 dimension_table_name="",
+                 dimension_table_insert_query="",
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
         # Map params here
         # Example:
         # self.conn_id = conn_id
-
+        self.redshift_conn_id = redshift_conn_id
+        self.dimension_table_name = dimension_table_name
+        self.dimension_table_insert_query = dimension_table_insert_query
     def execute(self, context):
-        self.log.info('LoadDimensionOperator not implemented yet')
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        dimension_sql = LoadDimensionOperator.dimension_sql_template.format(
+            dimension_table_name=self.dimension_table_name,
+            dimension_table_insert_query=self.dimension_table_insert_query
+        )
+        self.log.info("Run dimension table insert", dimension_sql)
+        redshift.run(dimension_sql)
--- data-pipeline/project/workspace/airflow/plugins/operators/load_fact.py
+++ data-pipeline/project/workspace/airflow/plugins/operators/load_fact.py
@@ -2,21 +2,36 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
-class LoadFactOperator(BaseOperator):
 
+class LoadFactOperator(BaseOperator):
     ui_color = '#F98866'
+    fact_sql_template = """
+        INSERT INTO {fact_table_name}
+        {fact_table_insert_query}
+    """
 
     @apply_defaults
     def __init__(self,
                  # Define your operators params (with defaults) here
                  # Example:
                  # conn_id = your-connection-name
+                 redshift_conn_id="",
+                 fact_table_name="",
+                 fact_table_insert_query="",
                  *args, **kwargs):
-
         super(LoadFactOperator, self).__init__(*args, **kwargs)
         # Map params here
         # Example:
         # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.fact_table_name = fact_table_name
+        self.fact_table_insert_query = fact_table_insert_query
 
     def execute(self, context):
-        self.log.info('LoadFactOperator not implemented yet')
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        fact_sql = LoadFactOperator.fact_sql_template.format(
+            fact_table_name=self.fact_table_name,
+            fact_table_insert_query=self.fact_table_insert_query
+        )
+        self.log.info("Run fact table insert", fact_sql)
+        redshift.run(fact_sql)
